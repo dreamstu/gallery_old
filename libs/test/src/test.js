69e7f4f2213e8663a8dcb196906d47c6f6e0573d
dreamstu <cjxsgn@163.com>
2014-12-10 18:45:40 +0800

    build

---
+++ libs/test/src/test.js
@@ -0,0 +1,13 @@
+/*
+ * test
+ * https://github.com/dreamstu/gallery
+ *
+ * Copyright (c) 2014 dreamstu
+ * Licensed under the MIT license.
+ */
+
+define(function(require, exports){
+	exports.hello = function(){
+		return 'hello';
+	};
+});
